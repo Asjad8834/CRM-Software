3968360f3d9121ad074375ac6fadbd372681b5bb
Asjad <ammarzain8834@gmail.com>
2026-04-09 00:03:31 +0530

    Fixed cors issue happening on the render servers as it was not allowing requests from the vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,16 @@ const interactionRoutes = require("./routes/interaction.route");
 const dashboardRoutes = require("./routes/dashboard.route");
 
 // Middleware
-app.use(cors());
+//to acces it from our render server we have to give it some parameters
+//we do it after both frontend and backend has been deployed 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://crm-software-plum.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 //Root route for showing on render
